0501f6dafdc90cd15d4c87fae62a79a86eaa2135
TommyRqn <tommy1712.RT@gmail.com>
2021-12-16 09:01:13 +0100

    Correction bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000 
 
-app.get('/' function(req, res){
+app.get('/', function(req, res){
     res.send("Hello World !")
 })
 
